de18ab0cefff29ae2c5d9cf528ae8d3eddf9872f
Danil Samsonov <thrashdev@protonmail.com>
2024-08-20 13:00:36 +0500

    implemented normalizeURL

--- crawl.js
+++ crawl.js
@@ -1,6 +1,15 @@
-
+const { log, error } = require('console')
 function normalizeUrl(urlString) {
-	return urlString
+	const url = new URL(urlString)
+	const host = url.host
+	let pathname = url.pathname
+	const lastChar = pathname.charAt(pathname.length - 1)
+	if (lastChar === '/') {
+		pathname = pathname.slice(0, -1);
+	}
+	const result = host + pathname
+	log(result)
+	return result
 }
 
 
